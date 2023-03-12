f05d4e171e31a7c7e85e3228b809154ba07cd56b
Bjørge Næss <bjoerge@gmail.com>
2023-03-12 13:07:57 +0100

    try work around import issue

--- meta.py
+++ meta.py
@@ -1,6 +1,8 @@
-import os
 
-[sysname, _, _, _, machine] = os.uname()
+def get_version():
+    VERSION = 0.3
 
-VERSION = meta.VERSION
-USER_AGENT = meta.USER_AGENT
+def get_user_agent():
+    import os
+    [sysname, _, _, _, machine] = os.uname()
+    USER_AGENT = f"usanity v{get_version()} / {sysname} - {machine}"
